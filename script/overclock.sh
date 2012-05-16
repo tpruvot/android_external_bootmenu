514e91d59a73deb9702da605a625eafb928ff288
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-05-16 19:47:15 +0200

    update sample overclock script for sio scheduler

--- script/overclock.sh
+++ script/overclock.sh
@@ -19,10 +19,12 @@ ASKED_MODE=$1
 # scaling 2
 # clk1 300
 # clk2 600
-# clk3 1000
+# clk3 800
+# clk4 1000
 # vsel1 30
 # vsel2 46
-# vsel3 58
+# vsel3 52
+# vsel4 58
 # con_up_threshold 80
 # con_down_threshold 20
 # con_freq_step 5
@@ -48,6 +50,7 @@ ASKED_MODE=$1
 # bst_sleep_wakeup_freq 300000
 # bst_up_rate_us 52000
 # bst_debug_mask 0
+# iosched_sio 0
 
 param_load()
 {
@@ -67,11 +70,11 @@ param_safe()
   export clk1=300
   export clk2=600
   export clk3=800
-  export clk4=1000
+  export clk4=800
   export vsel1=30
   export vsel2=46
   export vsel3=52
-  export vsel4=58
+  export vsel4=54
   export scaling=2
   export ond_up_threshold=86
   export ond_sampling_rate=50000
@@ -103,13 +106,13 @@ install_module()
     insmod $MODULE_DIR/cpufreq_powersave.ko
     insmod $MODULE_DIR/symsearch.ko
     insmod $MODULE_DIR/clockfix.ko
-    insmod $MODULE_DIR/sio-iosched.ko
     insmod $MODULE_DIR/cpufreq_stats.ko
     insmod $MODULE_DIR/cpufreq_interactive.ko
     insmod $MODULE_DIR/cpufreq_smartass.ko
     insmod $MODULE_DIR/cpufreq_boosted.ko
   fi
   busybox chown -R system /sys/devices/system/cpu
+  busybox chown -R system /sys/class/block/mmc*/queue
 }
 
 #############################################################
@@ -157,25 +160,9 @@ set_scaling()
     "5" )
       if [ "$load_all" -eq "0" ]; then
         insmod $MODULE_DIR/symsearch.ko
-        insmod $MODULE_DIR/sio-iosched.ko
         insmod $MODULE_DIR/clockfix.ko
         insmod $MODULE_DIR/cpufreq_boosted.ko
       fi
-
-      # options: 'noop cfq sio'
-      SCHED="sio"
-#     for i in /sys/block/mmc*/queue; do
-#       [ -f "$i/scheduler" ]             && echo $SCHED > $i/scheduler
-#       [ -f "$i/rotational" ]            && [ "`cat $i/rotational`" -ne "0" ] && echo 0 > $i/rotational
-#       [ -f "$i/iosched/low_latency" ]   && echo 1 > $i/iosched/low_latency
-#       [ -f "$i/iosched/back_seek_penalty" ] && echo 1 > $i/iosched/back_seek_penalty
-#       [ -f "$i/iosched/back_seek_max" ] && echo 1000000000 > $i/iosched/back_seek_max
-#       [ -f "$i/iosched/slice_idle" ]    && echo 0 > $i/iosched/slice_idle
-#       [ -f "$i/iosched/fifo_batch" ]    && echo 1 > $i/iosched/fifo_batch
-#       [ -f "$i/iosched/quantum" ]       && echo 16 > $i/iosched/quantum
-#       [ -f "$i/nr_requests" ]           && echo 512 > $i/nr_requests
-#       [ -f "$i/iostats" ]               && [ "`cat $i/iostats`" -ne "0" ] && echo 0 > $i/iostats
-#     done
 
       echo boosted > $SCALING_GOVERNOR
       usleep 200000
@@ -231,23 +218,61 @@ set_scaling()
 }
 
 #############################################################
+# Alternative I/O Schedulers
+#
+# Default linux I/O Schedulers are optimized for Hard disks
+# The alternative ones are optimized for flash disks
+#############################################################
+
+set_ioscheduler()
+{
+  iosched="cfq"
+  if [ "$iosched_sio" -eq "1" ]; then
+      iosched="sio"
+  fi
+
+  # options: 'noop cfq sio'
+  case "$iosched" in
+    "cfq" )
+    ;;
+    "sio" )
+      insmod $MODULE_DIR/sio_iosched.ko
+      for i in /sys/block/mmc*/queue; do
+        [ -f "$i/scheduler" ]                 && echo $iosched > $i/scheduler
+
+        [ -f "$i/iosched/low_latency" ]       && echo 1 > $i/iosched/low_latency
+        [ -f "$i/iosched/back_seek_penalty" ] && echo 1 > $i/iosched/back_seek_penalty
+        [ -f "$i/iosched/back_seek_max" ]     && echo 1000000000 > $i/iosched/back_seek_max
+        [ -f "$i/iosched/slice_idle" ]        && echo 0 > $i/iosched/slice_idle
+        [ -f "$i/iosched/fifo_batch" ]        && echo 1 > $i/iosched/fifo_batch
+        [ -f "$i/iosched/quantum" ]           && echo 16 > $i/iosched/quantum
+        [ -f "$i/nr_requests" ]               && echo 512 > $i/nr_requests
+
+        [ -f "$i/rotational" ]  && [ "`cat $i/rotational`" -ne "0" ] && echo 0 > $i/rotational
+        [ -f "$i/iostats" ]     && [ "`cat $i/iostats`" -ne "0" ]    && echo 0 > $i/iostats
+      done
+    ;;
+     * )
+    ;;
+  esac
+}
+
+#############################################################
 # Set Clock Table
 #############################################################
 
 set_overclock_table()
 {
-# echo "$vsel4" > /proc/overclock/max_vsel
-  echo "$vsel3" > /proc/overclock/max_vsel
-# echo "${clk4}000" > /proc/overclock/max_rate
-  echo "${clk3}000" > /proc/overclock/max_rate
-# echo "4 ${clk4}000000 $vsel4" > /proc/overclock/mpu_opps
+  echo "$vsel4" > /proc/overclock/max_vsel
+  echo "${clk4}000" > /proc/overclock/max_rate
+  echo "4 ${clk4}000000 $vsel4" > /proc/overclock/mpu_opps
   echo "3 ${clk3}000000 $vsel3" > /proc/overclock/mpu_opps
   echo "2 ${clk2}000000 $vsel2" > /proc/overclock/mpu_opps
   echo "1 ${clk1}000000 $vsel1" > /proc/overclock/mpu_opps
-# echo "0 ${clk4}000" > /proc/overclock/freq_table
-  echo "0 ${clk3}000" > /proc/overclock/freq_table
-  echo "1 ${clk2}000" > /proc/overclock/freq_table
-  echo "2 ${clk1}000" > /proc/overclock/freq_table
+  echo "0 ${clk4}000" > /proc/overclock/freq_table
+  echo "1 ${clk3}000" > /proc/overclock/freq_table
+  echo "2 ${clk2}000" > /proc/overclock/freq_table
+  echo "3 ${clk1}000" > /proc/overclock/freq_table
 }
 
 #############################################################
@@ -265,12 +290,18 @@ else
 fi
 
 if [ $enable -eq 1 ]; then
+
   get_address
   install_module
-  echo "set_scaling..."
+
+  echo "set scaling..."
   set_scaling
-  echo "set_overclock_table..."
+  echo "set overclock table..."
   set_overclock_table
+  echo "set ioscheduler..."
+  set_ioscheduler
+
   busybox chown -R system /sys/devices/system/cpu
+
 fi
 
